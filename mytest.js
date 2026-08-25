f8c4bd4f38561ebf1fb021b191c214b8c4dacf3c
Jashanpreet Singh <jashanpreetsingh3999@gmail.com>
2026-08-25 22:52:58 +0530

    uncached test files

--- mytest.js
+++
@@ -1,9 +0,0 @@
-const { urlsFromHTML } = require('./crawl.js')
-
-const url = 'https://www.apple.com/';
-(async () => {
-    let resp = await fetch(url);
-    resp = await resp.text()
-    let res = urlsFromHTML(resp, url)
-    console.log(res)
- })()
